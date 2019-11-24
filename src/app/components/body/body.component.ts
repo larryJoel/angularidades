bfd10e7011747a591bd9f371a38bfda6aa293d14
larryjoel <controlalo@outlook.com>
2019-11-24 19:23:28 -0200

    Cambios de ngFor / ngIf

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,4 +5,13 @@ import {Component} from '@angular/core';
 })
 export class BodyComponents {
 
+        mostrar = true;
+
+        frase: any ={
+            mensaje : '"Un gran poder amerita una gran responsabilidad"...',
+            autor : 'Ben Parquer'
+        }
+
+        personaje: string[]= ['Spiderman', 'Superman', 'Batman', 'Aquaman'];
+
 }
